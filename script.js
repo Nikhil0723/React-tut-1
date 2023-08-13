628b1cd0f7411ae854b6a67aeee95468c13fd6d3
Nikhil Vishwakarma <138506850+Nikhil0723@users.noreply.github.com>
2023-08-13 10:02:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const head = React.createElement("h1" , {} , 'heading1')
+const head2 = React.createElement("h1" , {} , 'heading2')
+
+const container = React.createElement("div" , {} , [head ,head2])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(container)
